00a0773762b30d2e7cda1542993e58e92443f581
Mushahadur Rahaman Khan <67184635+mushahadur@users.noreply.github.com>
2021-08-25 13:16:32 +0600

    Create even_odd.cpp

---
+++ BIT Manipulation/CUPC/even_odd.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a;
+    if((a&1) == 0)
+    {
+        cout<<"Even";
+    }
+    else
+    {
+        cout<<"Odd";
+    }
+    //cout<<char(1<<5);   /// Speces " "
+    //cout<<"Pulock"<<endl;
+    return 0;
+}
