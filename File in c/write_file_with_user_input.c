8f74341a1bf6758b579354ee9a92dfcff92a90cd
Mushahadur Rahaman Khan <67184635+mushahadur@users.noreply.github.com>
2021-09-24 12:53:11 +0600

    Create write_file_with_user_input.c

---
+++ File in c/write_file_with_user_input.c
@@ -0,0 +1,26 @@
+    /// Write file With user input
+#include<stdio.h>
+int main()
+{
+    FILE *file;
+    char name[100];
+    file = fopen("Myself.txt","w");     ///  a ->> when  overwrite, otherwise w.
+    if(file==NULL){
+        printf("File doesn't exist\n");
+    }
+    else{
+        printf("File is opened\n");
+        printf("Enter Your Full Name :");
+        gets(name);
+        fputc("\n",file);
+        fputs(name,file);
+        fputc("\n",file);
+        printf("File is written successfully\n");
+        fclose(file);
+    }
+
+
+
+    return 0;
+}
+
